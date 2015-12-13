45c1ef82a43ea45f64741768d93dbcff4c072c8c
Diana Iniestra <Diana-Iniestra@users.noreply.github.com>
2015-12-13 17:49:25 -0600

    Create plot1.R

---
+++ plot1.R
@@ -0,0 +1,11 @@
+# Explorarory Data Analysis
+# Project 1
+# Plot 1
+
+setwd("C:/Users/Diana/Desktop/Exploratory Data Analysis")
+library(dplyr)
+dataset <- read.table("household_power_consumption.txt", header = TRUE, sep = ";", na.strings = "?")
+dates <- filter(dataset, Date == "1/2/2007" | Date == "2/2/2007")
+hist(as.numeric(dates$Global_active_power), col = "red", main = "Global Active Power", xlab = "Global Active Power (kilowatts)", las = 1)
+dev.copy(png, "plot1.png")
+dev.off()
