aa4c416779545c128d64ce0d009700c20189224f
Diana Iniestra <Diana-Iniestra@users.noreply.github.com>
2015-12-13 18:25:25 -0600

    Update plot2.R

--- plot2.R
+++ plot2.R
@@ -2,7 +2,6 @@
 # Project 1
 # Plot 2
 
-setwd("C:/Users/Diana/Desktop/Exploratory Data Analysis")
 library(dplyr)
 dataset <- read.table("household_power_consumption.txt", header = TRUE, sep = ";", na.strings = "?")
 dates <- filter(dataset, Date == "1/2/2007" | Date == "2/2/2007")
